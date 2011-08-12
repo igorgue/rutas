e2191e44b4fac87754bbe4ee38f73bb406ddcebc
Igor Guerrero Fonseca <igfgt1@gmail.com>
2011-08-12 17:49:33 -0400

    Moar stuff

--- apps/search/features/index.py
+++ apps/search/features/index.py
@@ -1,8 +1,8 @@
-from lettuce import before, step, world
+from lettuce import after, before, step, world
 from lettuce.django import django_url
+from nose.tools import assert_equal, assert_true
 from splinter.browser import Browser
-
-from nose.tools import assert_true
+from termcolor import cprint
 
 @before.all
 def set_browser():
@@ -17,4 +17,18 @@ def given_i_access_the_url(step, url):
 
 @step(u'I see the header "(.*)"')
 def then_i_see_the_header(step, header):
-    assert_true(world.browser.is_text_present(header))
+    assert_equal(world.browser.find_by_css('h1').first.value, header)
+
+@after.all
+def unset_browser(total):
+    message = "{0} of {1} scenarios passed!".format(
+        total.scenarios_passed,
+        total.scenarios_ran
+    )
+
+    if total.scenarios_ran == total.scenarios_passed:
+        cprint("Congratulations! {0}".format(message), 'green')
+    else:
+        cprint("FAIL! {0}".format(message), 'red')
+
+    world.browser.quit()
